b9c70f8f2a96104950ef2a700cf1d74ec8e6da43
victor <victor@DESKTOP-ORPG0OS>
2022-08-21 10:49:37 -0300

    operações com numeros interios.

---
+++ jdevcursojava/src/jdevcursojava/OperacoesEntreNumerosInteiros.java
@@ -0,0 +1,14 @@
+package jdevcursojava;
+
+public class OperacoesEntreNumerosInteiros {
+	public static void main(String[] args) {
+			int nota1 = 90;	
+			int nota2 = 70;	
+			int nota3 = 40;	
+			int nota4 = 88;	
+			
+			System.out.println("Media das notas = "+ (nota1 + nota2+ nota3 + nota4)/4);
+			
+	}
+
+}
