943c8edbf7704e8392ccdf3f5d6427a218a6170f
victor <victor@DESKTOP-ORPG0OS>
2022-08-21 11:19:21 -0300

    Variaveis tipo DOUBLE.

---
+++ jdevcursojava/src/jdevcursojava/VariavelTipoDouble.java
@@ -0,0 +1,16 @@
+package jdevcursojava;
+
+public class VariavelTipoDouble {	
+		public static void main(String[] args) {
+			double nota1 = 90;	
+			double nota2 = 70;	
+			double nota3 = 50;	
+			double nota4 = 88;	
+			
+			double mediaFinal = (nota1 + nota2+ nota3 + nota4)/4;
+			System.out.println("Media das notas = " + mediaFinal);
+		}
+
+	}
+
+
