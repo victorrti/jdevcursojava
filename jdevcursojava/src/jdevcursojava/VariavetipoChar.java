6a02503e67dc91ffe6412ae42744fc2883457105
victor <victor@DESKTOP-ORPG0OS>
2022-08-21 11:29:06 -0300

    Variavel tipo char

---
+++ jdevcursojava/src/jdevcursojava/VariavetipoChar.java
@@ -0,0 +1,19 @@
+package jdevcursojava;
+
+public class VariavetipoChar {
+	public static void main(String[] args){
+		char pessoaFisicaJuridica = 'F';
+		char pessoaJuridica = 'J';
+		char sexo = 'M';
+		
+		if(pessoaFisicaJuridica == 'F') {
+			
+			System.out.println("Pessia Fisica");
+		} else {
+			System.out.println("Pessia Juridica");
+			
+		}
+		
+	}
+
+}
