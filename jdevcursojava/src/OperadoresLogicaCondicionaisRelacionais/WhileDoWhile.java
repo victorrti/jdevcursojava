e06c8aedd93975328a2e6feb82a264c6bbab2c86
victor <victor@DESKTOP-ORPG0OS>
2022-08-21 14:25:35 -0300

    repetição while , do while.

---
+++ jdevcursojava/src/OperadoresLogicaCondicionaisRelacionais/WhileDoWhile.java
@@ -0,0 +1,24 @@
+package OperadoresLogicaCondicionaisRelacionais;
+
+public class WhileDoWhile {
+	public static void main(String[] args) {
+		int contador =0;
+		while(contador < 20) {
+			contador++;
+			System.out.println(contador);
+			
+		}
+		System.out.println(contador);
+		int contador2 = 0;
+		do {
+			contador2++;
+			System.out.println(contador2);
+			
+		}while(contador2 < 20);
+		System.out.println(contador2);
+	}
+	
+	
+	
+
+}
