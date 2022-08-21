a9556530f90f736ed31bf2c9bd6c7dad238811db
victor <victor@DESKTOP-ORPG0OS>
2022-08-21 13:53:16 -0300

    comando switch case.

---
+++ jdevcursojava/src/OperadoresLogicaCondicionaisRelacionais/comandoSwitchCase.java
@@ -0,0 +1,30 @@
+package OperadoresLogicaCondicionaisRelacionais;
+
+public class comandoSwitchCase {
+	public static void main(String[] args) {
+		int opcaoramal = 2;
+
+		switch (opcaoramal) {
+		case 1:
+			System.out.println("setor tecnologia");
+			break;
+		case 2: 
+			System.out.println("setor suporte");
+			break;
+		case 3: 
+			System.out.println("setor financeiro");
+			break;
+		case 4: 
+			System.out.println("setor vendas");
+			break;
+		default :
+			System.out.println("opção invlida");
+			break;
+			
+		}
+
+	}
+	
+	
+   
+}
