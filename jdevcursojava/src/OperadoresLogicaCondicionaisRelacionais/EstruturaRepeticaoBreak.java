928afad8d865cdefd6098fbe270532cf250afa69
victor <victor@DESKTOP-ORPG0OS>
2022-08-21 14:43:07 -0300

    estrutura repeção for com break.

---
+++ jdevcursojava/src/OperadoresLogicaCondicionaisRelacionais/EstruturaRepeticaoBreak.java
@@ -0,0 +1,13 @@
+package OperadoresLogicaCondicionaisRelacionais;
+
+public class EstruturaRepeticaoBreak {
+	public static void main(String[] args) {
+		for(int numero = 0; numero <10; numero ++) {
+			if(numero == 6) {
+				System.out.println("numero 6");
+				break;
+			}
+		}
+	}
+   
+}
