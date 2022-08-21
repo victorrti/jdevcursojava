7cd243e299c1ddfa5f2183231b0e7b91ac99d772
victor <victor@DESKTOP-ORPG0OS>
2022-08-21 10:39:44 -0300

    variavel local e global.

---
+++ jdevcursojava/src/jdevcursojava/VaeriaveisLocalGlobal.java
@@ -0,0 +1,19 @@
+package jdevcursojava;
+
+public class VaeriaveisLocalGlobal {
+	/* Variavel Global e acessivel a todos */
+	static int MaiorIdadeGlobal = 30;
+	public static void main(String[] args) {
+		/* Variavel local pois pertence somente a este metodo*/
+		
+		int maiorIdade = 18;
+		System.out.println("variavel local :"+ maiorIdade);
+		
+		metodo2();		
+		
+	}
+	
+	public static void metodo2() { 
+		System.out.println("variavel global :"+ MaiorIdadeGlobal);
+	}
+}
