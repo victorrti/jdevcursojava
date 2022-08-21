e2a3fc7281919a267fa20e6fb4f1206527d66416
victor <victor@DESKTOP-ORPG0OS>
2022-08-21 14:01:17 -0300

    operadores relacionais;

---
+++ jdevcursojava/src/OperadoresLogicaCondicionaisRelacionais/OperadoresRelacionais.java
@@ -0,0 +1,27 @@
+package OperadoresLogicaCondicionaisRelacionais;
+
+public class OperadoresRelacionais {
+	public static void main(String[] args) {
+		double nota1 = 70;
+		double nota2 = 50;
+		
+		System.out.println(nota1==nota2);
+		System.out.println(nota1>nota2);
+		System.out.println(nota1>=nota2);
+		System.out.println(nota1<nota2);
+		System.out.println(nota1<=nota2);
+		System.out.println(nota1!=nota2);
+		
+		if(nota1>nota2) {
+			System.out.println("nota1 e  maior que nota 2");
+		}
+
+	}
+	
+	
+		
+		
+		
+		
+	
+}
