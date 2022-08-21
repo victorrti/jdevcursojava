13edc7251c9707015bac71cdb54ee380a1dd52a0
victor <victor@DESKTOP-ORPG0OS>
2022-08-21 14:34:33 -0300

    estrutura de repetição for

---
+++ jdevcursojava/src/OperadoresLogicaCondicionaisRelacionais/EstruturaRepeticaoFor.java
@@ -0,0 +1,16 @@
+package OperadoresLogicaCondicionaisRelacionais;
+
+public class EstruturaRepeticaoFor {
+	public static void main(String[] args) {
+		
+		for(int numero = 0; numero < 20 ; numero++) {
+			System.out.println("numero : "+ numero);
+		}
+		
+		for(int numero = 20; numero > 0 ; numero--) {
+			System.out.println("numero : "+ numero);
+		}
+		
+	}
+
+}
