68c091aa6784e2a3892c5c196c21e86abb98f892
victor <victor@DESKTOP-ORPG0OS>
2022-08-21 15:34:30 -0300

    operador MOD.

---
+++ jdevcursojava/src/OperadoresLogicaCondicionaisRelacionais/OperadorModoloMod.java
@@ -0,0 +1,10 @@
+package OperadoresLogicaCondicionaisRelacionais;
+
+public class OperadorModoloMod {
+	public static void main(String[] args) {
+		double carro = 9;
+		double pessoa = 2;
+		double resto = carro % pessoa;
+		System.out.println(resto);
+	}
+}
