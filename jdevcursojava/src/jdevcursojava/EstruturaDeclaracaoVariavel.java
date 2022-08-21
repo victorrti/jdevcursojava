0f476faed5537d41f53a8d30eb9e3b51f813565b
victor <victor@DESKTOP-ORPG0OS>
2022-08-21 10:08:58 -0300

    Estrutura e declaração der variaveis.

---
+++ jdevcursojava/src/jdevcursojava/EstruturaDeclaracaoVariavel.java
@@ -0,0 +1,22 @@
+package jdevcursojava;
+
+public class EstruturaDeclaracaoVariavel {
+	public static void main(String[] args) {
+		
+		int maiorIdade = 18;
+		
+		int idosoIdade = 60;
+		
+		int jovemIdade = 16;
+		
+		int criancaIdade = 7;
+		
+		String cpf = "123.456.789.12";
+		
+		boolean ecerto = true;
+		
+		float valor = 12.3f;
+		
+	}
+
+}
