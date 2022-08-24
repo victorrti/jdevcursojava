5922a55b01ecdb06e8d6d8ea4bdf61775ae3b6aa
victor <victor@DESKTOP-ORPG0OS>
2022-08-23 21:17:25 -0300

    Criação de metodos gets e setes. criando objeto e exibindo no console.

--- jdevcursojava/src/orientacaoObjeto/PriemieiraClasseAluno/Aluno.java
+++ jdevcursojava/src/orientacaoObjeto/PriemieiraClasseAluno/Aluno.java
@@ -1,15 +1,78 @@
 package orientacaoObjeto.PriemieiraClasseAluno;
 
 public class Aluno {
-	String nome;
-	int idade;
-	String dataNascimento;
-	String registroGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String serieMatriculaddo;
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+    private double nota;
+	
+	public double getNota() {
+		return nota;
+	}
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	public String getNomePai() {
+		return nomePai;
+	}
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+	public String getSerieMatriculaddo() {
+		return serieMatriculaddo;
+	}
+	public void setSerieMatriculaddo(String serieMatriculaddo) {
+		this.serieMatriculaddo = serieMatriculaddo;
+	}
+
+	private String nomePai;
+	private String dataMatricula;
+	private String serieMatriculaddo;
 	
 	
 	public Aluno() {		
--- jdevcursojava/src/orientacaoObjeto/PriemieiraClasseAluno/ClasseExecutavel.java
+++ jdevcursojava/src/orientacaoObjeto/PriemieiraClasseAluno/ClasseExecutavel.java
@@ -1,13 +1,20 @@
 package orientacaoObjeto.PriemieiraClasseAluno;
 
 public class ClasseExecutavel {
-	public static void mian(String[] args) {
+	public static void main(String[] args) {
+		 /* new aluno e uma instancia*/
 		Aluno aluno1 = new Aluno();
 		
 		/* construtor com parametro*/
 		Aluno aluno2 = new Aluno("Victor");
 		
-		System.out.println(aluno2);
+		System.out.println(aluno2.getNome());
+		
+		// setando valores e buscando 
+		
+		aluno1.setNomeMae("Edna");
+		System.out.println(aluno1.getNomeMae());
+		
 		
 	}
 
